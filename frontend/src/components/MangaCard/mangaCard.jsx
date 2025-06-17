54457bb3b211962476dcabb95b34776d138fd566
DagMihHieu <poekmini13@gmail.com>
2025-06-17 21:58:51 +0700

    hiển thị chương chính xác hơn

--- frontend/src/components/MangaCard/mangaCard.jsx
+++ frontend/src/components/MangaCard/mangaCard.jsx
@@ -167,7 +167,7 @@ const MangaCard = ({ manga, type = null, chapter = null, isFavorite = false, onR
                                         <div key={ch.id} className="chapter-item-card">
                                             <Link to={`/manga/${manga.id}/chapter/${ch.chapter_number}`} className="chapter-link">
                                                 <span className="chapter-number-card">Chương {ch.chapter_number}</span>
-                                                <span className="chapter-title-card">{ch.chapter_name || `Chapter ${ch.chapter_number}`}</span>
+                                                <span className="chapter-title-card">{ch.chapter_name || []}</span>
                                             </Link>
                                         </div>
                                     ))
